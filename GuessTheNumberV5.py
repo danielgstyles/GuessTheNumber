051eaf68880d062d00824b910c85a4a359d133e1
danielgstyles <daniel.g.styles@det.nsw.edu.au>
2024-03-25 14:44:22 +1100

    Added internal documentation (comments) to V5

--- GuessTheNumberV5.py
+++ GuessTheNumberV5.py
@@ -1,13 +1,13 @@
-# Guess the Number Game V3
+# Guess the Number Game V5
 # Creator: D Styles
 
-import os
+import os #This is a extra library that adds extra functions to Python. This will be used to clear the terminal screen when needed
 
 def pickOS():
     selectedOS = int(input("\n\nWhat Operating System are you running this game on? \n * Mac OS = 1 \n * Windows = 2 \n   "))
     return selectedOS
 
-def clearScreen(WhichOS):
+def clearScreen(WhichOS): #This function will select the correct clear screen command depending of the OS selcted
     if WhichOS == "Windows":
         os. system('cls')
     else:
@@ -15,7 +15,7 @@ def clearScreen(WhichOS):
 
 def CheckGuesses(setNumber):
     
-    guesses = []
+    guesses = [] #creates a blank list that will be used to keep track of all the guesses made by the player
     
     print("\n")
     guess = int(input("Player Two. Please enter your guess: "))
@@ -23,9 +23,9 @@ def CheckGuesses(setNumber):
     guesses.append(guess)
 
     print("\n")
-    print(guesses)
+    print(guesses) #debugging output statement to check that the list is being updated with the players guessed values
 
-    while numberToGuess != guess:
+    while numberToGuess != guess: #This loop will continue while ever the play guesses incorrectly
     
         if numberToGuess < guess:
             print("\nIt looks like your guess is too high. Tyr a lower number. \n")
@@ -59,7 +59,7 @@ while playAgain == "YES":
     whichOS = pickOS() #fills whichOS with the returned value from the pickOS function
     
     clearScreen(whichOS) #calls the clearScreen function passing the value held in the whichOS variable
-
+    
     print("Welcome to Guess The Number Game.\n")
 
     numberToGuess = int(input("Player 1 please set a number between 1 and 100 for PLayer 2 to guess. "))
