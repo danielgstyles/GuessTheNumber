bf8200fcac3e0ea61aa86cafcc64b8c46244d153
danielgstyles <daniel.g.styles@det.nsw.edu.au>
2024-03-25 20:14:19 +1100

    Begin Fixing appearance of menus/screens

--- GuessTheNumberV7.py
+++ GuessTheNumberV7.py
@@ -2,8 +2,8 @@
 # Creator: D Styles
 
 import os #This is a extra library that adds extra functions to Python. This will be used to clear the terminal screen when needed
-import random
-import time
+import random # This extra library adds the ability to create random numbers
+import time # This extra library adds the ability to pause the program for periods of time
 
 # def pickOS(): # Function no longer required as the os.name function identifies the OS
 #     selectedOS = int(input("\n\nWhat Operating System are you running this game on? \n * Mac OS = 1 \n * Windows = 2 \n   "))
@@ -20,17 +20,30 @@ def clearScreen(): #This function will select the correct clear screen command d
 
 
 def randomNumber():
-    difficulty = int(input("Easy = 1, Medium = 2, Hard = 3, Extra Hard = 4"))
+    clearScreen()
+    
+    print("\n  ******************************************************")
+    print("  *                                                    *")
+    print("  *    Please select your prefered difficulty level    *")
+    print("  *                                                    *")
+    print("  *                                                    *")
+    print("  *     1) Easy         A number between 1 and 10      *")
+    print("  *     2) Medium       A number between 1 and 50      *")
+    print("  *     3) Hard         A number between 1 and 100     *")
+    print("  *     4) Extra Hard   A number between 1 and 250     *\n")
+    difficulty = int(input("        Enter 1, 2, 3 or 4 to make your selection: "))
+    
+    
     if difficulty == 1:
-        ranNum = random.randint(0,10)
+        ranNum = random.randint(1,10)
     elif difficulty == 2:
-        ranNum = random.randint(0,50)
+        ranNum = random.randint(1,50)
     elif difficulty == 3:
-        ranNum = random.randint(0,100)
+        ranNum = random.randint(1,100)
     elif difficulty == 4:
-        ranNum = random.randint(0,250)
+        ranNum = random.randint(1,250)
     else:
-        print("\n Incorrect choise. Please try again.")
+        print("\n Incorrect choise. Please try again.") # This else is used to catch any other inputs other then the intended ones
         time.sleep(1)
         randomNumber()
 
@@ -48,8 +61,8 @@ def CheckGuesses(setNumber): # This function checks the Player 2 guesses against
 
     guesses.append(guess)
 
-    print("\n")
-    print(guesses) #debugging output statement to check that the list is being updated with the players guessed values
+    #print("\n")
+    #print(guesses) #debugging output statement to check that the list is being updated with the players guessed values
 
     while numberToGuess != guess: #This loop will continue while ever the play guesses incorrectly
     
@@ -61,8 +74,9 @@ def CheckGuesses(setNumber): # This function checks the Player 2 guesses against
 
         guesses.append(guess)
 
-        print("\n")
-        print(guesses)
+        #print("\n")
+        #print(guesses) #debugging output statement to check that the list is being updated with the players guessed valu15
+
 
 
     print("\n")
@@ -89,20 +103,30 @@ while playAgain == "YES" or playAgain == "Y": # added or "Y" just in case people
     #whichOS no longer needed as os.name can identify the OS and then the correct clear screen command can be used
     
     clearScreen() #calls the clearScreen function. This no longer needs the whichOS passed as the os.name is being used to automatically identify the OS fo the computer.
-    
-    print("Welcome to Guess The Number Game.\n")
+    print("")
+    print("****************************************************** \n")
+    print("           Welcome to Guess The Number Game.\n")
+    print("****************************************************** \n")
 
-    print("Would you like to play 1 Player of 2 Player?")
-    print("1 Player = 1 \n2 player = 2\n")
-    numberOfPlayers = int(input())
+    print("      Would you like to play 1 Player of 2 Player?\n")
+    print("                   1)   1 Player")
+    print("                   2)   2 player \n")
+    numberOfPlayers = int(input("         Enter 1 or 2 to make your selection:"))
+    
+    
     if numberOfPlayers == 2:
-        numberToGuess = int(input("Player 1 please set a number between 1 and 100 for PLayer 2 to guess. "))
+        clearScreen()
+        print("******************************************************")
+        print("              Player 1 please set a number ")
+        print("         between 1 and 100 for PLayer 2 to guess.")
+        print("****************************************************** \n")
+        numberToGuess = int(input("          Slected Number: "))
     else:
         numberToGuess = randomNumber()
         
     clearScreen()
     
-    print(numberToGuess) # debugging output statement.
+    #print(numberToGuess) # debugging output statement.
 
     playerName = CheckGuesses(numberToGuess)
     
